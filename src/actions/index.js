34b80c6e5c07c8a903b1c03b05aa0417cc7cec49
Joe Lagasse <joe@servant.co>
2016-04-04 11:44:12 -0400

    Add and implement redux-form. Can now post to db

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 //You do not have to register for an API key. Just Make one up '?key=anything'
@@ -10,6 +11,15 @@ export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
   return {
     type: FETCH_POSTS,
+    payload: request
+  };
+}
+
+export function createPost(props) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+  
+  return {
+    type: CREATE_POST,
     payload: request
   };
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,35 @@
 import React, {Component} from 'react';
+import {reduxForm} from 'redux-form';
+import {createPost} from '../actions/index';
 
 class PostsNew extends Component {
-    render() {
-        return (
-          <div>Create form</div>  
-        );
-    }
+  render() {
+    const {fields: {title, categories, content}, handleSubmit} = this.props;
+    return (
+      <form onSubmit={handleSubmit(this.props.createPost)}>
+        <h3>Create a New Post</h3>
+        <div className='form-group'>
+          <label>Title</label>
+          <input type='text' className='form-control' {...title} />
+        </div>
+
+        <div className='form-group'>
+          <label>Categories</label>
+          <input type='text' className='form-control' {...categories} />
+        </div>
+
+        <div className='form-group'>
+          <label>Content</label>
+          <textarea className='form-control' {...content} />
+        </div>
+
+        <input type='submit' className='btn btn-primary' value='submit' />
+      </form>
+    );
+  }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNew',
+  fields: ['title', 'categories', 'content']
+}, null, {createPost})(PostsNew);
