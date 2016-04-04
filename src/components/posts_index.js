209cbed5cda2fa7a544d16cb849070d990eae81c
Joe Lagasse <joe@servant.co>
2016-04-04 18:52:10 -0400

    Set up link and action when targeting specific posts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -12,8 +12,10 @@ class PostsIndex extends Component {
     return this.props.posts.map((post) => {
       return (
         <li className='list-group-item' key={post.id}>
-          <span className='pull-xs-right'>{post.categories}</span>
-          <strong>{post.title}</strong>
+          <Link to={'posts/' + post.id}>
+            <span className='pull-xs-right'>{post.categories}</span>
+            <strong>{post.title}</strong>
+          </Link>
         </li>
       );
     });
@@ -38,7 +40,6 @@ class PostsIndex extends Component {
 }
 
 function mapStateToProps(state) {
-  console.log(state.posts.all);
   return {posts: state.posts.all};
 }
 
